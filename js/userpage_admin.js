446a07c5c4f8fc033c39a019bb5914ec46cc07f9
mentosberi <92323467+mentosberi@users.noreply.github.com>
2025-03-10 21:04:44 +0800

    Added my files  (wip) - Yesha

---
+++ js/userpage_admin.js
@@ -0,0 +1,15 @@
+function confirmation() {
+  if (confirm("Are you sure?")) {
+    document.getElementById("Option").innerText = "User deleted!";
+  }
+}
+
+///document.getElementById("acc-options").style.display = "none";
+
+
+/*search function*/
+
+
+/*delete function*/
+
+
